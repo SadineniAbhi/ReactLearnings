0d7b473d8e19c8821d35ec7cef540a4d4478994f
SadineniAbhi <abhisadineni@gmail.com>
2023-11-13 00:54:06 +0530

    added home page

---
+++ bookstore/src/components/Homepage.js
@@ -0,0 +1,59 @@
+import React from 'react'
+
+export default function Homepage() {
+  return (
+    <div>
+        <header>
+            <h1>Welcome to Book Store</h1>
+            <p>📚 Discover, Read, and Explore 📚</p>
+        </header>
+        <main>
+            <section>
+                <h2>What We Offer</h2>
+                <ul>
+                    <li>
+                        <h3>Diverse Book Collection</h3>
+                        <p>Our shelves are stacked with a wide array of books. From gripping novels and timeless classics to non-fiction treasures, we've got it all. You'll find books from renowned authors and promising new talents alike.</p>
+                    </li>
+                    <li>
+                        <h3>Reading Convenience</h3>
+                        <p>Enjoy the convenience of reading. Choose from hardcovers, paperbacks, eBooks, or even audiobooks. Whether you love the feel of a physical book or prefer the portability of a digital edition, we've got you covered.</p>
+                    </li>
+                    <li>
+                        <h3>Great Prices</h3>
+                        <p>We believe that reading should be affordable for all. That's why we offer competitive prices and exciting discounts on our books. Quality reading material doesn't have to break the bank.</p>
+                    </li>
+                    <li>
+                        <h3>Stay Updated</h3>
+                        <p>Subscribe to our newsletter to stay in the loop about the latest releases, special promotions, and literary events. Don't miss out on our exciting offers and updates.</p>
+                    </li>
+                </ul>
+            </section>
+            <section>
+                <h2>Why Choose Us?</h2>
+                <ul>
+                    <li>
+                        <h3>Quality Selection</h3>
+                        <p>We carefully curate our collection to ensure the highest quality and relevance to our readers.</p>
+                    </li>
+                    <li>
+                        <h3>Convenient Delivery</h3>
+                        <p>Enjoy prompt and reliable book delivery right to your doorstep.</p>
+                    </li>
+                    <li>
+                        <h3>Passion for Reading</h3>
+                        <p>We're book enthusiasts ourselves, and our passion reflects in our selection and service.</p>
+                    </li>
+                    <li>
+                        <h3>Expert Advice</h3>
+                        <p>Our team is always ready to assist you in finding the perfect book.</p>
+                    </li>
+                </ul>
+            </section>
+        </main>
+        <footer>
+            <p>&copy; Book Store| Contact Us: [Your Contact Information]</p>
+        </footer>
+    </div>
+  )
+}
